c7d8a7458ed854681220a06603c073a284d09918
Alexander Litovkin <litovkinalexan@gmail.com>
2024-08-28 16:59:18 +0700

    docs: add documentation for post-service controller

--- post-service/src/main/java/com/allitov/controller/PostController.java
+++ post-service/src/main/java/com/allitov/controller/PostController.java
@@ -13,6 +13,10 @@ import org.springframework.web.bind.annotation.*;
 import java.net.URI;
 import java.util.UUID;
 
+/**
+ * Rest controller for post service.
+ * @author Alexander Litovkin
+ */
 @Slf4j
 @RestController
 @RequestMapping("/api/v1/post")
@@ -20,6 +24,10 @@ public final class PostController {
 
     // post-resource
 
+    /**
+     * Endpoint for obtaining all data about posts.
+     * @return an HTTP response.
+     */
     @GetMapping
     public ResponseEntity<PostListResponse> getAllPosts() {
         log.info("Get all posts request");
@@ -27,6 +35,11 @@ public final class PostController {
         return ResponseEntity.ok().build();
     }
 
+    /**
+     * Endpoint for obtaining post data by ID.
+     * @param postId an ID by which to find a post.
+     * @return an HTTP response.
+     */
     @GetMapping("/{postId}")
     public ResponseEntity<PostResponse> getPostById(@PathVariable("postId") UUID postId) {
         log.info("Get post by id request with id = '{}'", postId);
@@ -34,6 +47,11 @@ public final class PostController {
         return ResponseEntity.ok().build();
     }
 
+    /**
+     * Endpoint for creating post.
+     * @param requestBody a request body with post data.
+     * @return an HTTP response.
+     */
     @PostMapping
     public ResponseEntity<PostResponse> createPost(@RequestBody PostRequest requestBody) {
         log.info("Create post request with body = '{}'", requestBody);
@@ -41,6 +59,12 @@ public final class PostController {
         return ResponseEntity.created(URI.create("/api/v1/post/" + UUID.randomUUID())).build();
     }
 
+    /**
+     * Endpoint for updating post data by ID.
+     * @param postId an ID by which to find a post.
+     * @param requestBody a request body with post data.
+     * @return an HTTP response.
+     */
     @PutMapping("/{postId}")
     public ResponseEntity<Void> updatePostById(@PathVariable("postId") UUID postId,
                                                @RequestBody PostRequest requestBody) {
@@ -49,6 +73,11 @@ public final class PostController {
         return ResponseEntity.noContent().build();
     }
 
+    /**
+     * Endpoint for deleting post data by ID.
+     * @param postId an ID by which to find a post.
+     * @return an HTTP response.
+     */
     @DeleteMapping("/{postId}")
     public ResponseEntity<Void> deletePostById(@PathVariable("postId") UUID postId) {
         log.info("Delete post by id request with id = '{}'", postId);
@@ -58,6 +87,11 @@ public final class PostController {
 
     // comment-resource
 
+    /**
+     * Endpoint for obtaining all data about post comments.
+     * @param postId an ID by which to find a post.
+     * @return an HTTP response.
+     */
     @GetMapping("/{postId}/comment")
     public ResponseEntity<CommentListResponse> getAllPostComments(@PathVariable("postId") UUID postId) {
         log.info("Get all post comments request with post id = '{}'", postId);
@@ -65,6 +99,12 @@ public final class PostController {
         return ResponseEntity.ok().build();
     }
 
+    /**
+     * Endpoint for obtaining post comment data by ID.
+     * @param postId an ID by which to find a post.
+     * @param commentId an ID by which to find a comment.
+     * @return an HTTP response.
+     */
     @GetMapping("/{postId}/comment/{commentId}")
     public ResponseEntity<CommentResponse> getPostCommentById(@PathVariable("postId") UUID postId,
                                                               @PathVariable("commentId") UUID commentId) {
@@ -73,6 +113,12 @@ public final class PostController {
         return ResponseEntity.ok().build();
     }
 
+    /**
+     * Endpoint for creating post comment.
+     * @param postId an ID by which to find a post.
+     * @param requestBody a request body with comment data.
+     * @return an HTTP response.
+     */
     @PostMapping("/{postId}/comment")
     public ResponseEntity<Void> createPostComment(@PathVariable("postId") UUID postId,
                                                   @RequestBody CommentRequest requestBody) {
@@ -81,6 +127,13 @@ public final class PostController {
         return ResponseEntity.created(URI.create("/api/v1/post/comment/" + UUID.randomUUID())).build();
     }
 
+    /**
+     * Endpoint for updating post comment data by ID.
+     * @param postId an ID by which to find a post.
+     * @param commentId an ID by which to find a comment.
+     * @param requestBody a request body with comment data.
+     * @return an HTTP response.
+     */
     @PutMapping("/{postId}/comment/{commentId}")
     public ResponseEntity<Void> updatePostCommentById(@PathVariable("postId") UUID postId,
                                                       @PathVariable("commentId") UUID commentId,
@@ -91,6 +144,12 @@ public final class PostController {
         return ResponseEntity.noContent().build();
     }
 
+    /**
+     * Endpoint for deleting post comment data by ID.
+     * @param postId an ID by which to find a post.
+     * @param commentId an ID by which to find a comment.
+     * @return an HTTP response.
+     */
     @DeleteMapping("/{postId}/comment/{commentId}")
     public ResponseEntity<Void> deletePostCommentById(@PathVariable("postId") UUID postId,
                                                       @PathVariable("commentId") UUID commentId) {
