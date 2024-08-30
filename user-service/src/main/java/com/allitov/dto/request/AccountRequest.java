3735bdccad1735859510cf07fd9c322fb22a9dd0
Alexander Litovkin <litovkinalexan@gmail.com>
2024-08-30 17:50:59 +0700

    docs: add documentation for user-service dto classes

--- user-service/src/main/java/com/allitov/dto/request/AccountRequest.java
+++ user-service/src/main/java/com/allitov/dto/request/AccountRequest.java
@@ -3,6 +3,10 @@ package com.allitov.dto.request;
 import lombok.Builder;
 import lombok.Data;
 
+/**
+ * Dto class for account create/update request.
+ * @author Alexander Litovkin
+ */
 @Data
 @Builder
 public class AccountRequest {
--- user-service/src/main/java/com/allitov/dto/response/AccountResponse.java
+++ user-service/src/main/java/com/allitov/dto/response/AccountResponse.java
@@ -5,6 +5,10 @@ import lombok.Data;
 
 import java.util.UUID;
 
+/**
+ * Dto class for account response.
+ * @author Alexander Litovkin
+ */
 @Data
 @Builder
 public class AccountResponse {
